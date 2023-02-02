bf02248d57cc061243b35fa99a297d5a175d3994
Oliver Payne <oliverpayne@Olivers-MBP.domain>
2023-02-01 21:39:35 -0700

    progress

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,42 @@
+/** @jsxImportSource theme-ui */
+import { useState, useEffect } from "react"
+
 const Header = () => {
-    return (
-        <header>
-            
-        </header>
-    )
+  const [hasScrolled, setHasScrolled] = useState(false)
+  useEffect(() => {
+    document.addEventListener("scroll", () => {
+      if (window.scrollY !== 0) {
+        setHasScrolled(true)
+      } else {
+        setHasScrolled(false)
+      }
+    })
+  }, [])
+
+  return (
+    <header
+      sx={{
+        height: hasScrolled ? "80px" : "60px",
+        position: "sticky",
+        top: 0,
+        backgroundColor: "#F8F8F8",
+        display: "flex",
+        boxShadow: "0px 0px 8px rgb(0 0 0 / 20%)",
+        transition: "all 0.2s ease-out",
+        transitionDelay: "0.05s"
+      }}
+    >
+      <nav>
+        <ul
+          sx={{
+            listStyleType: "none"
+          }}
+        >
+          <li></li>
+        </ul>
+      </nav>
+    </header>
+  )
 }
 
 export default Header
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,23 +1,34 @@
-import Header from 'components/Header'
-import SEO from 'components/SEO'
-import Head from 'next/head'
-import Footer from 'components/Footer'
+/** @jsxImportSource theme-ui */
+import Header from "components/Header"
+import SEO from "components/SEO"
+import Head from "next/head"
+import Footer from "components/Footer"
 
 export default function Home() {
   return (
     <>
       <Head>
-        <SEO title="Ollie Payne - SEO Analyst" metaDescription='Description' />
+        <SEO
+          title="Ollie Payne - Digital Marketer"
+          metaDescription="Description"
+        />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Header />
-      <main>
+      <main
+        sx={{
+          minHeight: "1000px"
+        }}
+      >
         <section>
-          
+          <h2>About me</h2>
         </section>
         <section>
-
+          <h2>My Work</h2>
+        </section>
+        <section>
+          <h2>Get in Touch</h2>
         </section>
       </main>
       <Footer />
