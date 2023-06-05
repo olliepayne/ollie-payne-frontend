0bbc93f5ad52814eda03af345d53e7af2d9e4b58
Oliver Payne <olliepayne@me.com>
2023-06-04 23:12:15 -0700

    progress

--- src/pages/blog/[slug].tsx
+++ src/pages/blog/[slug].tsx
@@ -2,8 +2,9 @@
 import Layout from "components/Layout"
 import SEO from "components/SEO"
 import Image from "next/image"
-import { Box, Container, Heading, Text } from "theme-ui"
+import { Box, Container, Heading, Text, Paragraph } from "theme-ui"
 import ReactMarkdown from "react-markdown"
+import { theme } from "theme"
 
 import { blogPosts } from "../../../public/blogPosts.json"
 const { pageTitle, metaDescription, h1, datePublished } = blogPosts[0]
@@ -16,20 +17,35 @@ import markdown from "../../../public/markdown.md"
 //   h1: string
 // }
 
+interface ICustomComponentProps {
+  children: React.ReactNode | React.ReactNode[]
+}
+const components = {
+  p: Paragraph,
+  h2: ({ children }: ICustomComponentProps) => (
+    <Heading as="h2" variant="styles.h2">
+      {children}
+    </Heading>
+  ),
+  h3: ({ children }: ICustomComponentProps) => (
+    <Heading as="h3" variant="styles.h3">
+      {children}
+    </Heading>
+  )
+}
+
 const BlogPostPage = () => {
   return (
     <Layout>
       <SEO title={pageTitle} metaDescription={metaDescription} />
-      <article
-        sx={{
-          minHeight: "3000px"
-        }}
-      >
+      <article>
         {/* metadata / frontmatter */}
         <section
-          sx={{
-            // py: 4
-          }}
+          sx={
+            {
+              // py: 4
+            }
+          }
         >
           <Container variant="narrow">
             <Heading as="h1" variant="styles.h1">
@@ -68,7 +84,7 @@ const BlogPostPage = () => {
         {/* markdown / blog post content */}
         <section>
           <Container variant="narrow">
-            <ReactMarkdown>{markdown}</ReactMarkdown>
+            <ReactMarkdown components={components}>{markdown}</ReactMarkdown>
           </Container>
         </section>
       </article>
