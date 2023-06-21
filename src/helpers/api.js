c5df48ddcfdfc51d28cc5aa95a9aae99336e0f2d
Oliver Payne <olliepayne@me.com>
2023-06-21 14:51:19 -0700

    progress

--- src/helpers/api.js
+++ src/helpers/api.js
@@ -1,7 +1,8 @@
 export const getStrapiUrl = () => {
   const env = process.env.NODE_ENV
   if (env === "development") {
-    return "http://192.168.0.3:1337"
+    return "http://localhost:1337"
+    // return process.env.STRAPI_URL
     // return process.env.STRAPI_URL
   } else if (env === "production") {
     return process.env.PROD_STRAPI_URL
