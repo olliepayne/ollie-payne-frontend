b08485f68ec4fa9fb2ac3d757b0ae76c9d93f409
Oliver Payne <olliepayne@me.com>
2023-07-10 13:11:16 -0700

    Add: children prop

--- src/components/SEO.tsx
+++ src/components/SEO.tsx
@@ -1,17 +1,19 @@
 import Head from "next/head"
 
-interface ISEO {
+type Props = {
   title: string
   metaDescription: string
+  children?: React.ReactNode | React.ReactNode[]
 }
 
-const SEO = ({ title, metaDescription }: ISEO) => {
+const SEO = ({ children, title, metaDescription }: Props) => {
   return (
     <Head>
       <meta name="viewport" content="width=device-width, initial-scale=1" />
       <link rel="icon" href="/favicon.ico" />
       <title>{title}</title>
       <meta name="description" content={metaDescription} />
+      {children}
     </Head>
   )
 }
