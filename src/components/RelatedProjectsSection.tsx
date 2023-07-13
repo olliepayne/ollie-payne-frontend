cf81f0722c37a70c77119e77ca6e3c88eec98e2b
Oliver Payne <olliepayne@me.com>
2023-07-12 22:54:52 -0700

    Add: background color

--- src/components/RelatedProjectsSection.tsx
+++ src/components/RelatedProjectsSection.tsx
@@ -18,7 +18,8 @@ const RelatedProjectsSection = ({ projects }: Props) => {
   return (
     <section
       sx={{
-        py: [4, 5]
+        py: [4, 5],
+        bg: "myPink"
       }}
     >
       <Container>
